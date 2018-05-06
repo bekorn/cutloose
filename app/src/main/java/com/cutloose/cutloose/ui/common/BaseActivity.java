66f18e25c46856d498785f2079ff9ec1e58fcf61
Barkin Monster <barkinkaplan@gmail.com>
2018-05-06 05:05:05 +0300

    Edited base activity in order to remove status bar. Edited styles.xml to remove title. Created log in page. Added font

--- app/src/main/java/com/cutloose/cutloose/ui/common/BaseActivity.java
+++ app/src/main/java/com/cutloose/cutloose/ui/common/BaseActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     public void onCreate( @Nullable Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView( getViewId() );
     }
--- app/src/main/java/com/cutloose/cutloose/ui/login/LoginActivity.java
+++ app/src/main/java/com/cutloose/cutloose/ui/login/LoginActivity.java
@@ -1,12 +1,35 @@
 package com.cutloose.cutloose.ui.login;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
 import com.cutloose.cutloose.R;
+import com.cutloose.cutloose.ui.chat.ChatActivity;
 import com.cutloose.cutloose.ui.common.BaseActivity;
 
 public class LoginActivity extends BaseActivity {
 
+
+    @Override
+    public void onCreate( @Nullable Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        TextView signUp = findViewById(R.id.login_activity_text_sign_up);
+        signUp.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView forgotPassword = findViewById(R.id.login_activity_text_forgot_password);
+        forgotPassword.setMovementMethod(LinkMovementMethod.getInstance());
+    }
     @Override
     protected int getViewId() {
         return R.layout.login_activity;
+    }
+
+    public void logIn(View view){
+        Intent intent = new Intent(this, ChatActivity.class);
+        startActivity(intent);
     }
 }
