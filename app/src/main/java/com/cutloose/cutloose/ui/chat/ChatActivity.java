e078ea26b988e7809528cc2aebf35a246fd91a86
Berk Emre Saribas <berksaribas@sabanciuniv.edu>
2018-05-06 20:27:57 +0300

    Implemented a fake loading screen for finding chat.

--- app/src/main/java/com/cutloose/cutloose/ui/chat/ChatActivity.java
+++ app/src/main/java/com/cutloose/cutloose/ui/chat/ChatActivity.java
@@ -1,9 +1,43 @@
 package com.cutloose.cutloose.ui.chat;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+
 import com.cutloose.cutloose.R;
+import com.cutloose.cutloose.databinding.ChatActivityBinding;
 import com.cutloose.cutloose.ui.common.BaseActivity;
 
 public class ChatActivity extends BaseActivity {
+
+    ChatActivityBinding chatActivityBinding;
+    ChatActivityViewModel chatActivityViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        chatActivityViewModel = ViewModelProviders.of(this).get(ChatActivityViewModel.class);
+
+        chatActivityBinding = (ChatActivityBinding) binding;
+        chatActivityBinding.setViewModel(chatActivityViewModel);
+        chatActivityViewModel.searching.set(true);
+        setFakeTimer();
+    }
+
+    private void setFakeTimer() {
+        new CountDownTimer(3000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                chatActivityViewModel.searching.set(false);
+            }
+
+        }.start();
+    }
 
     @Override
     protected int getViewId() {
---
+++ app/src/main/java/com/cutloose/cutloose/ui/chat/ChatActivityViewModel.java
@@ -0,0 +1,12 @@
+package com.cutloose.cutloose.ui.chat;
+
+
+import android.databinding.ObservableBoolean;
+
+import com.cutloose.cutloose.ui.common.BaseViewModel;
+
+public class ChatActivityViewModel extends BaseViewModel {
+    public ObservableBoolean searching = new ObservableBoolean();
+
+
+}
