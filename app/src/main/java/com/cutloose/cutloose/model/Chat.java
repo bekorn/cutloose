25dcb7c9aefdf1af1712b2b9649a73ce5eadbdb4
Barkin Monster <barkinkaplan@gmail.com>
2018-05-06 20:45:34 +0300

    chat model

---
+++ app/src/main/java/com/cutloose/cutloose/model/Chat.java
@@ -0,0 +1,45 @@
+package com.cutloose.cutloose.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by finge on 5/6/2018.
+ */
+
+public class Chat {
+    private String id;
+    private Event eventType;
+    private ArrayList<Profile> owners;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setEventType(Event eventType) {
+        this.eventType = eventType;
+    }
+
+    public void setOwners(ArrayList<Profile> owners) {
+        this.owners = owners;
+    }
+
+    public String getId() {
+
+        return id;
+    }
+
+    public Event getEventType() {
+        return eventType;
+    }
+
+    public ArrayList<Profile> getOwners() {
+        return owners;
+    }
+
+    public Chat(String id, Event eventType, ArrayList<Profile> owners) {
+
+        this.id = id;
+        this.eventType = eventType;
+        this.owners = owners;
+    }
+}
