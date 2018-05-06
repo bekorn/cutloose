dc40e55b00fc9869a1a425523621feab6dd9012b
Berk Emre Saribas <berksaribas@sabanciuniv.edu>
2018-05-06 20:27:27 +0300

    Implemented on click for event items.

--- app/src/main/java/com/cutloose/cutloose/ui/event/EventItemViewModel.java
+++ app/src/main/java/com/cutloose/cutloose/ui/event/EventItemViewModel.java
@@ -1,6 +1,11 @@
 package com.cutloose.cutloose.ui.event;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
 import com.cutloose.cutloose.model.Event;
+import com.cutloose.cutloose.ui.chat.ChatActivity;
 import com.cutloose.cutloose.ui.common.BaseViewModel;
 
 public class EventItemViewModel extends BaseViewModel {
@@ -11,5 +16,13 @@ public class EventItemViewModel extends BaseViewModel {
         super();
 
         mEvent = event;
+    }
+
+    public void onClickItem(View v) {
+        //TODO: This process should be done in Activity instead of View Model
+        Context context = v.getContext();
+
+        Intent intent = new Intent(context, ChatActivity.class);
+        context.startActivity(intent);
     }
 }
