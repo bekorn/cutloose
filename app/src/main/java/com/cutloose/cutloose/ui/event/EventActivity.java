ab543d668919fcdc07adc88edcceb1c38d754404
Berk Canli <berkcanli@sabanciuniv.edu>
2018-05-08 01:38:09 +0300

    EventActivity now uses BaseRecyclerViewAdapter's onClick handling

--- app/src/main/java/com/cutloose/cutloose/ui/event/EventActivity.java
+++ app/src/main/java/com/cutloose/cutloose/ui/event/EventActivity.java
@@ -1,5 +1,6 @@
 package com.cutloose.cutloose.ui.event;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,14 +12,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.cutloose.cutloose.R;
+import com.cutloose.cutloose.model.Event;
+import com.cutloose.cutloose.ui.chat.ChatActivity;
 import com.cutloose.cutloose.ui.common.BaseActivity;
 import com.cutloose.cutloose.ui.profile.ProfileActivity;
 
 public class EventActivity extends BaseActivity {
 
     static private final String TAG = "EventActivity";
-
-    private EventRecyclerViewModel mEventRecyclerViewModel;
 
     @Override
     protected int getViewId() {
@@ -29,39 +30,49 @@ public class EventActivity extends BaseActivity {
     public void onCreate( @Nullable Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
 
-        setTitle(R.string.event_title);
+        setTitle( R.string.event_title );
 
-        mEventRecyclerViewModel = ViewModelProviders.of( this ).get( EventRecyclerViewModel.class );
+        EventRecyclerViewModel eventRecyclerViewModel = ViewModelProviders.of( this ).get( EventRecyclerViewModel.class );
+
+        EventRecyclerViewAdapter eventRecyclerViewAdapter = new EventRecyclerViewAdapter( eventRecyclerViewModel, this );
+
+        eventRecyclerViewAdapter.mOnItemClickEvent.observe( this, new Observer<Event>() {
+            @Override
+            public void onChanged( @Nullable Event event ) {
+                Intent intent = new Intent( EventActivity.this, ChatActivity.class );
+                startActivity( intent );
+            }
+        } );
 
         RecyclerView recyclerView = findViewById( R.id.event_recycler_view );
 
         recyclerView.setLayoutManager( new GridLayoutManager( this, 3 ) );
-        recyclerView.setAdapter( new EventRecyclerViewAdapter( mEventRecyclerViewModel, this ) );
+        recyclerView.setAdapter( eventRecyclerViewAdapter );
 
-        mEventRecyclerViewModel.fetchData();
+        eventRecyclerViewModel.fetchData();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu( Menu menu ) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.titlebar_menu, menu);
+        menuInflater.inflate( R.menu.titlebar_menu, menu );
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+    public boolean onOptionsItemSelected( MenuItem item ) {
+        switch( item.getItemId() ) {
             case R.id.menu_profile:
                 startProfileActivity();
                 return true;
             default:
-                return super.onOptionsItemSelected(item);
+                return super.onOptionsItemSelected( item );
         }
 
     }
 
-    private void startProfileActivity(){
-        Intent intent = new Intent(this, ProfileActivity.class);
-        startActivity(intent);
+    private void startProfileActivity() {
+        Intent intent = new Intent( this, ProfileActivity.class );
+        startActivity( intent );
     }
 }
